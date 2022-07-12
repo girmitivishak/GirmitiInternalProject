848a3d4eb60852b9240f3d13db7cfe6423e03d04
nilesh <nilesh.powar@girmiti.com>
2022-07-12 13:08:58 +0530

    Dir structure changed & added seperate stylesheet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
-import Signin from './screens/signin/Signin';
+import Signin from './screens/signin/index';
 
 function App() {
   return(
