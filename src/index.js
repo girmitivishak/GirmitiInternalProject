993e618af5e6387b746e9b6d9303cc2644cc0bcd
GirmitiUddeepan <uddeepan.sahoo@girmiti.com>
2022-07-19 12:17:11 +0530

    changed app.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {View} from 'react-native';
 import BottomTabNav from './screens/BottomTabNav';
-import Signin from './screens/signin';
+// import Signin from './screens/signin';
 
 function App() {
   return (
-    <View>
-      {/* <Signin /> */}
-      <BottomTabNav />
-    </View>
+    // <Signin />
+    <BottomTabNav />
   );
 }
 
